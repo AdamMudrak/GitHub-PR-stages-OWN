4a99af892479ab862da170e63d454ee89d922a21
AdamMudrak <mudrets2000@gmail.com>
2023-01-06 15:03:50 +0200

    Add addition function

--- src/pushed_app/Main.java
+++ src/pushed_app/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My pull request practice app.");
     }
+
+    public static double Addition(double a, double b) {
+        return a + b;
+    }
 }
